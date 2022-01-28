8595a7590de3f2b853a92cceea16f46f39c7cf70
LanaGV <ymorok@yandex.ru>
2022-01-28 18:17:06 +0300

    Add homework_4

---
+++ task_2.2.py
@@ -0,0 +1,7 @@
+
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
+for i in my_list:
+    if i.isdigit() or i.isalnum() is False:
+        if j.isdigit() in i:
+            j
