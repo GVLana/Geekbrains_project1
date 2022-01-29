35410eb295959179331e6a78ef3aeee3db71f72f
LanaGV <ymorok@yandex.ru>
2022-01-29 21:54:45 +0300

    Add homework 2. Task_2.2 has a solution for task_2.3

--- task_2.2.py
+++ task_2.2.py
@@ -1,7 +1,13 @@
 
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
+
 for i in my_list:
     if i.isdigit() or i.isalnum() is False:
-        if j.isdigit() in i:
-            j
+        if len(i) == 1:
+            i = f'"{int(i):02}"'
+        elif len(i) > 1 and i[0].isdigit() is False:
+            i = f'"{i[0]}{int(i[1:]):02}"'
+        elif i.isdigit() and len(i) > 1:
+            i = f'"{i}"'
+    print(i, end=" ")
